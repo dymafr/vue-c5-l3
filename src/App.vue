4cbc9130a60c06351e2540065d7ecc944f10d0bd
dymafr <erwan1309@hotmail.com>
2022-02-23 16:41:26 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,12 @@
 <template>
-  <input
-    class="input"
-    :class="{ inputOngoing, inputError, inputValid }"
-    type="text"
-    @focus="
-      input.focus = true;
-      input.touched = true;
-    "
-    @blur="input.focus = false"
-    v-model="input.value"
-  />
+  <h1 :style="[fontSize, color]">Bonjour le monde !</h1>
 </template>
 
 <script setup lang="ts">
-import { reactive, computed } from 'vue';
+import { ref } from 'vue';
 
-const input = reactive({
-  value: '',
-  focus: false,
-  touched: false,
-});
-
-const inputOngoing = computed(() => input.focus && input.value.length);
-const inputError = computed(
-  () => !input.focus && input.touched && input.value.length < 5
-);
-const inputValid = computed(
-  () => !input.focus && input.touched && !inputError.value
-);
+const fontSize = ref('font-size: 50px');
+const color = ref('color: red');
 </script>
 
-<style scoped>
-input {
-  outline: 0;
-  border: 2px solid black;
-  border-radius: 4px;
-}
-.inputOngoing {
-  border-color: blue;
-}
-.inputError {
-  border-color: red;
-}
-.inputValid {
-  border-color: green;
-}
-</style>
+<style scoped></style>
